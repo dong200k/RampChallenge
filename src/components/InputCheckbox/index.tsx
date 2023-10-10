6e78b03e86c2adddb91cb5895e8931e9f025ccdb
dong200k <dchen5678@gmail.com>
2023-10-09 23:26:56 -0400

    first commit

---
+++ src/components/InputCheckbox/index.tsx
@@ -0,0 +1,33 @@
+import classNames from "classnames"
+import { useRef } from "react"
+import { InputCheckboxComponent } from "./types"
+
+export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, disabled, onChange }) => {
+  const { current: inputId } = useRef(`RampInputCheckbox-${id}`)
+  
+  /* Bug 2 fix */
+  const onClickCheckBox = () => {
+    onChange(!checked)
+  }
+
+  return (
+    <div className="RampInputCheckbox--container" onClick={onClickCheckBox} data-testid={inputId}>
+      <label
+        className={classNames("RampInputCheckbox--label", {
+          "RampInputCheckbox--label-checked": checked,
+          "RampInputCheckbox--label-disabled": disabled,
+        })}
+      />
+      <input
+        id={inputId}
+        type="checkbox"
+        className="RampInputCheckbox--input"
+        // checked={checked}
+        disabled={disabled}
+        // onChange={() => {
+        //   onChange(!checked)
+        // }}
+      />
+    </div>
+  )
+}
---
+++ src/components/Transactions/index.tsx
@@ -0,0 +1,37 @@
+import { useCallback } from "react"
+import { useCustomFetch } from "src/hooks/useCustomFetch"
+import { SetTransactionApprovalParams } from "src/utils/types"
+import { TransactionPane } from "./TransactionPane"
+import { SetTransactionApprovalFunction, TransactionsComponent } from "./types"
+
+export const Transactions: TransactionsComponent = ({ transactions, isLoading }) => {
+  const { fetchWithoutCache, loading } = useCustomFetch()
+
+  const setTransactionApproval = useCallback<SetTransactionApprovalFunction>(
+    async ({ transactionId, newValue }) => {
+      await fetchWithoutCache<void, SetTransactionApprovalParams>("setTransactionApproval", {
+        transactionId,
+        value: newValue,
+      })
+    },
+    [fetchWithoutCache]
+  )
+
+  /** New Bug fix, refer to notes.txt for howto reproduce. */
+  if (transactions === null || isLoading) {
+    return <div className="RampLoading--container">Loading...</div>
+  }
+
+  return (
+    <div data-testid="transaction-container">
+      {transactions.map((transaction) => (
+        <TransactionPane
+          key={transaction.id}
+          transaction={transaction}
+          loading={loading}
+          setTransactionApproval={setTransactionApproval}
+        />
+      ))}
+    </div>
+  )
+}
